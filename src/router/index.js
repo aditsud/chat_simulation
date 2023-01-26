3bf7c4097d6a4cb58f043ac1b314a05a833249cc
Macbook Kantor 202165 <mac202165@Macbook-Kantor-202165.local>
2023-01-26 07:46:08 +0700

    change router to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { getStorage } from '../config/storage';
 
 let notLogin = async (to, from, next) => {
@@ -12,7 +12,7 @@ let notLogin = async (to, from, next) => {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '',
